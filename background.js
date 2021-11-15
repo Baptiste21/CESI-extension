5635d0eed18b7866a94dd4010325e43cec2af9a6
Baptiste21 <44480098+Baptiste21@users.noreply.github.com>
2021-11-15 09:38:36 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
 chrome.runtime.onInstalled.addListener(function(){
     alert("CesiTools: Passe un bon moment sur l'extention. ");
 });
-chrome.webNavigation.onCompleted.addListener(function(){
-    alert("Une mauvaise journée s'annonce.... Bon courage!");
-}, {url:[{urlMatches:"ent.cesi.fr"}]});
